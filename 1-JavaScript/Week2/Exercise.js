fa0c44fc472bb606b968af3f3a3cc1f08d086dd2
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-05-25 00:53:38 +0200

    Exercise.js is deleted

--- 1-JavaScript/Week2/Exercise.js
+++
@@ -1,9 +0,0 @@
-let array1=[['a','b', 3 , 5,'Beyza'],[true,false, 7, 8.5],[6, 8,'text', 2, 5,'Amer']];
-let array2=[...arr];
-for (let i = 0; i < array1.length; i++) {
-for (let j = 0  ; j <array1[i].length; j++) {
-   if(typeof(array1[i][j])==='number') {
-    array2.push(array1[i][j]);}
-}
-}
-console.log(array2);
