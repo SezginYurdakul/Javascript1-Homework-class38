348127ce78c0dab76d0c9bc6df36a0aadd9f3ea4
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-05-25 00:50:44 +0200

    Week01 and week02 are added this repo

---
+++ 1-JavaScript/Week2/PrepExercise/1-remove-the-comma.js
@@ -0,0 +1,29 @@
+/**
+ * We want to remove the comma's in the given string (myString), replace them with a space and log it to the console.
+ * 
+ * The end result should be: 
+ *   hello this is a difficult to read sentence
+ */
+
+
+ let myString = 'hello,this,is,a,difficult,to,read,sentence';
+  myString = myString.replaceAll(","," ");
+  console.log(myString);
+
+
+
+ //Second solution
+
+ let myStringArray = myString.split(",");
+ const myStringArraylength=myStringArray.length;
+
+ for (let i = 0; i < myStringArraylength; i++) {
+    
+    myString = myString.replace(","," ");
+}
+ console.log(myString);
+
+
+ /* --- Code that will test your solution, do NOT change. Write above this line --- */
+ 
+ console.assert(myString === 'hello this is a difficult to read sentence', 'There is something wrong with your solution');
---
+++ 1-JavaScript/Week2/PrepExercise/5-who-wants-a-drink.js
@@ -0,0 +1,25 @@
+const drinkTypes = ['cola', 'lemonade', 'water'];
+const drinkTray=[];
+
+
+for (let i = 0; i < drinkTypes.length; i++) {
+    for (let j = 0; j < 2; j++) {
+        if (drinkTray.length < 5) {
+            drinkTray.push(drinkTypes[i]);
+        }
+    }
+}
+
+console.log("Hey guys, I brought a " +  drinkTray.join(', '));
+
+
+/**
+ * You're at a party and you feel thirsty! However, you've got 5 friends who are also in need of a drink. Let's go get them a drink.
+ *
+ * Declare a variable that holds an empty array, called drinkTray.
+ * Create a loop that runs 5 times. On each iteration, push a drink into the drinkTray variable. The drinkTray can only hold at most two instances of the same drink type, for example it can only hold 2 colas, 2 lemonades, 2 waters.
+ * 
+ * Log to the console: "Hey guys, I brought a [INSERT VALUES FROM ARRAY]!" (For example: "Hey guys, I brought a cola, cola, lemonade, lemonade, water!")
+ */
+
+// There are 3 different types of drinks:
