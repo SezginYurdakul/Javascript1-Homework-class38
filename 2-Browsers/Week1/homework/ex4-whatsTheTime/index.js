fef7ba661166bab2a8875f90c016a1f6cbad5870
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-06-14 01:33:58 +0200

    ex4 is done

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,8 +7,30 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
   second). Use `setInterval()` to make sure the time stays current.
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
+const h1 = document.createElement('h1');
+const textNode = document.createTextNode('Digital Watch : ');
+h1.appendChild(textNode);
+document.body.appendChild(h1);
+
+const h2 = document.createElement('h2');
+document.body.appendChild(h2);
 function addCurrentTime() {
   // TODO complete this function
+  let date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+
+  hrs = hrs < 10 ? '0' + hrs : hrs;
+  mins = mins < 10 ? '0' + mins : mins;
+  secs = secs < 10 ? '0' + secs : secs;
+
+  let time = `${hrs}:${mins}:${secs}`;
+  document.querySelector('H2').textContent = time;
+  setTimeout(addCurrentTime, 1000);
 }
 
 // TODO execute `addCurrentTime` when the browser has completed loading the page
+window.addEventListener('load', (event) => {
+  addCurrentTime();
+});
